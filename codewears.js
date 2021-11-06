8fe4503536a65b4e4309f677bffb27c2e01c665e
Mikhail Shestak <stampout386@gmail.com>
2021-11-06 18:19:35 +0300

    formft code

--- codewears.js
+++ codewears.js
@@ -1,10 +1,12 @@
 function anagrams(word, words) {
     const length = word.length;
     const sample = word.split('').sort().join('');
-    return words.filter(w => w.length === length)
+    return words
+        .filter((w) => w.length === length)
         .filter(function (e) {
             return e.split('').sort().join('') === sample;
-        })
+        });
 }
 
-
+let words = ['ehlol', 'loleh', 'hhhrd'];
+console.log(anagrams('hello', words));
