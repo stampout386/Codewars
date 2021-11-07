630f0abbf2bb93fd09818a374cdb6ef1854cdfee
Mikhail Shestak <stampout386@gmail.com>
2021-11-07 11:21:55 +0300

    findUnique

--- codewears.js
+++ codewears.js
@@ -10,3 +10,29 @@ function anagrams(word, words) {
 
 let words = ['ehlol', 'loleh', 'hhhrd'];
 console.log(anagrams('hello', words));
+
+let arr = [1, 8, 4, 4, 6, 1, 8, 22, 22, 44, 44, 57, 57];
+
+function findUnique(numbers) {
+    numbers.sort();
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] != numbers[i - 1] && numbers[i] != numbers[i + 1]) {
+            console.log(numbers[i]);
+            return numbers[i];
+        }
+    }
+}
+
+findUnique(arr);
+
+function findUnique2(numbers) {
+    const store = numbers.reduce((a, b) => {
+        !a[b] ? (a[b] = 1) : a[b]++;
+        return a;
+    }, {});
+    console.log(store);
+    let c = +Object.keys(store).filter((key) => store[key] === 1);
+    return c;
+}
+
+findUnique2(arr);
