1136d5409d9e647c65a224d87127598a5084cbf3
Mikhail Shestak <stampout386@gmail.com>
2021-11-08 16:54:30 +0300

    sum intervals

--- codewears.js
+++ codewears.js
@@ -118,3 +118,32 @@ function DNAStrand(dna) {
 }
 
 DNAStrand(dna);
+
+function sumIntervals(intervals) {
+    intervals = intervals.sort(function (a, b) {
+        // O(n)
+        return a[0] - b[0];
+    });
+    intervals = intervals.reduce(function (acc, el, index, array) {
+        // O(n)
+        const anterior = array[index - 1];
+        if (array.length > 1 && anterior !== undefined) {
+            if (el[0] < acc[acc.length - 1]) {
+                if (el[1] >= acc[acc.length - 1]) {
+                    acc[acc.length - 1] = el[1];
+                }
+            } else {
+                acc.push(...el);
+            }
+        } else {
+            acc.push(...el);
+        }
+        return acc;
+    }, []);
+    let result = 0;
+    for (let i = 0; i < intervals.length - 1; i += 2) {
+        // O(2n)
+        result += intervals[i + 1] - intervals[i];
+    }
+    return result;
+}
